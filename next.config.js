bca7f8e83658d4a5836b22d0dec9e16183acce0a
Collins Kibet <kibetchumba276@gmail.com>
2026-03-18 15:35:31 +0300

    Fix Netlify build: exclude Supabase Edge Functions from Next.js build

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    // Exclude Supabase Edge Functions from the build
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/supabase/functions/**', '**/node_modules/**']
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
